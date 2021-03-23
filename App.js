a60057b50e277e4af7e8751d948fbe4cbca2d8ad
zahidyew <zahidyew96@gmail.com>
2021-03-24 02:14:13 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.text}>Hello World</Text>
+      <Text> Counter: {counter} </Text>
+      <Button title="Click me" onPress={() => setCounter(counter + 1)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 24,
+  }
+});
+
+export default App;
